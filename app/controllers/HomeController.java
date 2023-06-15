635bd85bd3feaad890c332ea7e4968a40bea4561
Sebastián Gallegos F <59512190+SebaGallegos@users.noreply.github.com>
2023-06-15 13:34:56 -0400

    Creacion de base de datos

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,6 +8,10 @@ import play.i18n.MessagesApi;
 
 import javax.inject.Inject;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+
 public class HomeController extends Controller{
 
     private final Form<UserProcess> form;
@@ -17,6 +21,20 @@ public class HomeController extends Controller{
     public HomeController(FormFactory formFactory, MessagesApi messagesApi){
         this.form = formFactory.form(UserProcess.class);
         this.messagesApi = messagesApi;
+    }
+
+    private void createDB() throws SQLException{
+        File dbFile = new File("/home/sebastian/ejemplo-play/app/models/database.db");
+        if (!dbFile.exists()){
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/sebastian/ejemplo-play/app/models/database.db")){
+                String createTable = "CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT), name TEXT, email TEXT)";
+                try (Statement statement = connection.createStatement()){
+                    statement.executeUpdate(createTable);
+                }
+            } catch (SQLException e){
+                throw e;
+            }
+        }
     }
 
     public Result index(){
@@ -36,9 +54,30 @@ public class HomeController extends Controller{
             UserProcess userProcess = boundForm.get();
             User user = new User(userProcess.getName(), userProcess.getEmail());
 
-            System.out.println(user);
-
             return ok("Ingresado");
         }
+    }
+
+    public Result createUser(Http.Request request){
+        try {
+            createDB();
+            String name = request.body().asFormUrlEncoded().get("name")[0];
+            String email = request.body().asFormUrlEncoded().get("email")[0];
+
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/sebastian/ejemplo-play/app/models/database.db")) {
+                String query = "INSERT INTO usuarios (name, email) VALUES (?,?);";
+                try (PreparedStatement statement = connection.prepareStatement(query)) {
+                    statement.setString(1, name);
+                    statement.setString(2, email);
+
+                    statement.executeUpdate();
+                }
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            return internalServerError("Error al crear la base de datos");
+        }
+
+        return redirect(routes.HomeController.index());
     }
 }
--- build.sbt
+++ build.sbt
@@ -8,3 +8,6 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.13.11"
 
 libraryDependencies += guice
+
+// https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.42.0.0"
