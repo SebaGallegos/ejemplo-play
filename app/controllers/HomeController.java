42c0ae3bae23147886a4d40acf55c8361a51554b
Sebastián Gallegos F <59512190+SebaGallegos@users.noreply.github.com>
2023-06-15 17:18:49 -0400

    Base de datos usando Ebeans

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,35 +8,15 @@ import play.i18n.MessagesApi;
 
 import javax.inject.Inject;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.*;
-
 public class HomeController extends Controller{
 
     private final Form<UserProcess> form;
     private MessagesApi messagesApi;
-    private final String dbPath = new File("app/models/database.db").getAbsolutePath();
-    private final String jdbc = "jdbc:sqlite:" + dbPath;
 
     @Inject
     public HomeController(FormFactory formFactory, MessagesApi messagesApi){
         this.form = formFactory.form(UserProcess.class);
         this.messagesApi = messagesApi;
-    }
-
-    private void createDB() throws SQLException{
-        File dbFile = new File(dbPath);
-        if (!dbFile.exists()){
-            try (Connection connection = DriverManager.getConnection(jdbc)){
-                String createTable = "CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT), name TEXT, email TEXT)";
-                try (Statement statement = connection.createStatement()){
-                    statement.executeUpdate(createTable);
-                }
-            } catch (SQLException e){
-                throw e;
-            }
-        }
     }
 
     public Result index(){
@@ -47,38 +27,16 @@ public class HomeController extends Controller{
         return ok(views.html.form.render(form, request, messagesApi.preferred(request)));
     }
 
-    public Result submitForm(Http.Request request){
-        final Form<UserProcess> boundForm = form.bindFromRequest(request);
-
-        if (boundForm.hasErrors()){
-            return badRequest();
-        } else{
-            UserProcess userProcess = boundForm.get();
-            User user = new User(userProcess.getName(), userProcess.getEmail());
-
-            return ok("Ingresado");
-        }
-    }
-
     public Result createUser(Http.Request request){
-        try {
-            createDB();
-            String name = request.body().asFormUrlEncoded().get("name")[0];
-            String email = request.body().asFormUrlEncoded().get("email")[0];
+        String name = request.body().asFormUrlEncoded().get("name")[0];
+        String email = request.body().asFormUrlEncoded().get("email")[0];
 
-            try (Connection connection = DriverManager.getConnection(jdbc)) {
-                String query = "INSERT INTO usuarios (name, email) VALUES (?,?);";
-                try (PreparedStatement statement = connection.prepareStatement(query)) {
-                    statement.setString(1, name);
-                    statement.setString(2, email);
+        User user = new User();
 
-                    statement.executeUpdate();
-                }
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-            return internalServerError("Error al crear la base de datos");
-        }
+        user.name = name;
+        user.email = email;
+
+        user.save();
 
         return redirect(routes.HomeController.index());
     }
--- app/models/User.java
+++ app/models/User.java
@@ -1,16 +1,19 @@
 package models;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import io.ebean.Finder;
+import io.ebean.Model;
+
+@Entity
+public class User extends Model{
+
+    @Id
+    public int id;
+
     public String name;
     public String email;
 
-    public User(String name, String email){
-        this.name = name;
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Nombre: " + this.name + ", Email: " + this.email;
-    }
+    public static final Finder<Long, User> find = new Finder<>(User.class);
 }
